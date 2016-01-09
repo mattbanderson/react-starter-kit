ddf628bad1ecfe52fd61b6472437d2bcca1fc269
mbanderson3 <mbanderson33@gmail.com>
2016-01-09 17:26:58 -0500

    Add basic react-router component.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'babel-core/polyfill';
 ﻿import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import configureStore from './configureStore';
 import App from './containers/app';
 
@@ -10,7 +11,11 @@ const store = configureStore();
 
 render(
   <Provider store={store}>
-    <App />
+      <Router history={browserHistory}>
+        <Route path="/" component={App}>
+          // Additional routes here
+        </Route>
+      </Router>
   </Provider>,
   document.getElementById('root')
 );
